40b51f62e313f5d927ce440324f5aa1767da07a8
siewyanhoh <siew.yan.hoh@cern.ch>
2020-12-20 16:40:35 +0100

    added gen level variables

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -4,6 +4,8 @@ import os, sys
 import time
 from collections import OrderedDict
 
+ROOT.ROOT.EnableImplicitMT(12)
+
 cwd = os.getcwd()
 lumi="35.92"
 #lumi="41.53"
@@ -16,7 +18,7 @@ print("lumi : ", lumi )
 print("wp : ", wp )
 print("commonMC : ", commonMC )
 
-dfout = TFile( "WHSS_histogram.root" , "recreate" )
+dfout = TFile( "WHSS_reco.root" , "recreate" )
 t_top =	dfout.mkdir("whss")
 
 # hard-coded variables                                                                                                                                                                   
@@ -26,7 +28,12 @@ def PrepareVariable( df_in , name ):
     tfolder.cd()
     
     # Define
-    df_in = df_in.Define( 'Lepton1_eta' , 'Lepton_eta[0]' ).Define( 'Lepton2_eta' , 'Lepton_eta[1]' ).Define( 'Lepton1_pt' , 'Lepton_pt[0]' ).Define( 'Lepton2_pt' , 'Lepton_pt[1]' )
+    df_in = df_in\
+            .Define( 'Lepton1_eta' , 'Lepton_eta[0]' ).Define( 'Lepton2_eta' , 'Lepton_eta[1]' )\
+            .Define( 'Lepton1_pt' , 'Lepton_pt[0]' ).Define( 'Lepton2_pt' , 'Lepton_pt[1]' )\
+            .Define( 'deltaLep_Eta'      , 'abs(Lepton_eta[0] - Lepton_eta[1])' )\
+            .Define( 'deltaCleanJet_Eta' , 'abs(CleanJet_eta[0] - CleanJet_eta[1])' )\
+            .Define( 'deltaLep_Phi'      , 'abs(Lepton_eta[0] - Lepton_eta[1])' )
 
     h = OrderedDict() ; weight_ = 'weights'
     h['Lepton1_eta']  = df_in.Histo1D( ( 'Lepton1_eta'  , '%s ; Lepton1 eta        ; Events' %( name ) , 10 , -2.5 , 2.5 ) , 'Lepton1_eta'  , weight_ )
@@ -36,12 +43,21 @@ def PrepareVariable( df_in , name ):
     h['mlljj20_whss'] = df_in.Histo1D( ( 'mlljj20_whss' , '%s ; mlljj20_whss [GeV] ; Events' %( name ) , 50 , 0. , 200.  ) , 'mlljj20_whss' , weight_ )
     
     map(lambda x: h[x].Write() , h)
-    print("Final yield : %.2f" %df_in.Sum(weight_).GetValue() )
     t_top.cd()
+pass
+
+def report( df_in ):
+    report = df_in.Report()
+    report.Print()
+    print(" --> Final yields : %.2f" %df_in.Sum('weights').GetValue() )
+    print("")
 pass
 
 df = ROOT.ROOT.RDataFrame( "Signals" , [ "../processor/output/sshwminus012j.root" , "../processor/output/sshwplus012j.root" ] )
 df = df.Define( "weights" , commonMC )
+
+########### no selection
+PrepareVariable( df , "Raw" )
 
 ########### presel
 print(" --> Preselection <--")
@@ -51,6 +67,7 @@ df_presel = df\
             .Filter( "bVeto==1", "bVeto==1" )\
             .Filter( "PuppiMET_pt > 30" , "PuppiMET_pt > 30" )
 PrepareVariable( df_presel , "Preselection" )
+report( df_presel )
 
 ########### e-mu 1jet
 print(" --> e-mu 1jet <-- " )
@@ -62,6 +79,7 @@ df_emu1jet = df_presel\
              .Filter( "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" , "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" )\
              .Filter( "mlljj20_whss > 50" , "mlljj20_whss > 50" )
 PrepareVariable( df_emu1jet , "emu1jet" )
+report( df_emu1jet )
 
 ########### e-mu 2jet
 print(" --> e-mu 2jet <-- " )
@@ -74,6 +92,7 @@ df_emu2jet = df_presel\
              .Filter( "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" , "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" )\
              .Filter( "mlljj20_whss > 50" , "mlljj20_whss > 50" )
 PrepareVariable( df_emu2jet , "emu2jet" )
+report( df_emu2jet )
 
 ########### mu-mu 1jet
 print(" --> mu-mu 1jet <-- " )
@@ -86,6 +105,7 @@ df_mumu1jet = df_presel\
              .Filter( "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" , "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" )\
              .Filter( "mlljj20_whss > 50" , "mlljj20_whss > 50" )
 PrepareVariable( df_mumu1jet , "mumu1jet" )
+report( df_mumu1jet )
 
 ########### mu-mu 2jet
 print(" --> mu-mu 2jet <-- " )
@@ -99,6 +119,7 @@ df_mumu2jet = df_presel\
              .Filter( "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" , "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" )\
              .Filter( "mlljj20_whss > 50" , "mlljj20_whss > 50" )
 PrepareVariable( df_mumu2jet , "mumu2jet" )
+report( df_mumu2jet )
 
 ########### e-e 1jet
 print(" --> e-e 1jet <-- " )
@@ -111,6 +132,7 @@ df_ee1jet = df_presel\
              .Filter( "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" , "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" )\
              .Filter( "mlljj20_whss > 50" , "mlljj20_whss > 50" )
 PrepareVariable( df_ee1jet , "ee1jet" )
+report( df_ee1jet )
 
 ########### e-e 2jet
 print(" --> e-e 2jet <-- " )
@@ -124,5 +146,6 @@ df_ee2jet = df_presel\
              .Filter( "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" , "abs(Lepton_eta[0] - Lepton_eta[1])<2.0" )\
              .Filter( "mlljj20_whss > 50" , "mlljj20_whss > 50" )
 PrepareVariable( df_ee2jet , "ee2jet" )
+report( df_ee2jet )
 
 dfout.Close()
